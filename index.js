c1a64c7b74bbf2f1a58f5298e9c653779ec6812e
vincenbryaniel@gmail.com <vincenbryaniel@gmail.com>
2022-06-25 23:55:47 +0300

    commit "code clean up"

--- index.js
+++ index.js
@@ -12,24 +12,7 @@ function darkMode() {
 
   }
 
-// let name =document.getElementsById(`name`).value;
-// console.log(name)
-// let helpBot = ()=>{
-// const url = `https://enigmatic-coast-25791.herokuapp.com/softwares ${name}`
-// fetch(url)
-// .then(response => response.json())
-//   .then(response => console.log(response));
-// }
 
-
-// gethelpBot();
-// let name=document.getElementById("name");
-// console.log(name);
-// let getSoftware =()=>{
-//   console.log("Software")
-
-// }
-// getSoftware();
 
 const search =document.getElementById('search');
 const matchList= document.getElementById('match-list')
@@ -78,33 +61,8 @@ let searchSoftwares = () => {
           
           })
   // get matches to current text input
+  
 }
-
-// //search  softwares.json and filiter it
-// let matches = data.filter(software =>{
-//   const regex = newRegExp(`^${searchText}`,'gi');
-//   return search.name.match(regex) || software.description.match(regex);
-// });
-// if (searchText.length === 0) {
-//   matches = [];
-// }
-// // console.log(matches);
-
-// //show results in html
-// const outputHtml = matches =>{
-//   if(matches.length > 0){
-//     const html = matches.map(match =>
-//       `<div class="card card-body mb-1">
-//       <h4>${match.name}(${match.description}) <span class="text-primary">${match.capital}</span></h4>
-//       <small>author:${match.version}/version:${match.version}</small>
-//       </div> `
-//        ).join(" ");
-//       //  console.log(html)
-//       matchList.innerHTML =html;
-//   }
-// }
-
-// search.addEventListener('input', ()=> searchSoftwares(search.value));
 
 let initialize = () => {
         searchSoftwares();
